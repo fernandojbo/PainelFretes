26a974a2310201b73664845ca86e07dd43f27127
fernandojbo <114361129+fernandojbo@users.noreply.github.com>
2025-11-16 07:09:30 -0300

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-blue-700 text-white p-4 shadow-md fixed top-0 left-0 w-full z-50">
+      <div className="max-w-5xl mx-auto flex justify-between">
+        <h1 className="text-xl font-bold">Painel de Fretes</h1>
+      </div>
+    </header>
+  );
+}
