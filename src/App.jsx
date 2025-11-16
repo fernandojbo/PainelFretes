c277b13249219ab4fa63cc62afde5d71db6adbb1
fernandojbo <114361129+fernandojbo@users.noreply.github.com>
2025-11-16 07:19:37 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
+export default function App() {
+  const [logged, setLogged] = useState(false);
+
+  return (
+    <>
+      <Header />
+      {!logged ? <Login onLogin={setLogged} /> : <Dashboard />}
+      <Footer />
+    </>
+  );
+}
